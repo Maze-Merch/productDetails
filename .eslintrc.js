9cfdde51b8be1ea129b50159a0556e2e635247a3
Zachery Converse <zacheryconverse@gmail.com>
2020-06-16 20:23:27 -0600

    configure airBnB esLinter, .babelrc, and eslintrc.json.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,8 @@
  *   https://github.com/reactorcore/eslint-config-hackreactor
  */
 
-module.exports = {
-  extends: './node_modules/eslint-config-hackreactor/index.js'
-};
+// module.exports = {
+//   extends: './node_modules/eslint-config-hackreactor/index.js'
+// };
+
+// Commented out because .eslintrc.json may be the replacement for this.
