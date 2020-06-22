6697b5448b216bbf42080dd789f7cdc50853b4fc
Zachery Converse <zacheryconverse@gmail.com>
2020-06-22 15:51:06 -0600

    reset develop branch

--- client/src/components/Details.jsx
+++ client/src/components/Details.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
+import App from './App';
 
-class Details extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
-  }
-
-  render() {
-    console.log('details props', this.props.products)
-    // const { products } = props.products;
-    return (
-      <div>
-        {/* <h3>{this.props.products[0]}</h3> */}
-      </div>
-    );
-  }
-};
+const Details = ({ products }) => (
+//   <div>
+//     {products.map(({ id, ...product }) => (
+//       // console.log(product)
+//       <div key={id} {...product} />
+//       // <div key={id} {product.category} />
+//     ))}
+//   </div>
+  <div className="content">
+    <ul className="products">
+      <li>
+        <div className="product">
+          <div className="product-category">Kicks</div>
+          <div className="product-name">Heir Force Ones</div>
+          <div className="price">$99</div>
+          <img src="https://images.unsplash.com/photo-1544441892-794166f1e3be?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80" alt="white shoes" />
+        </div>
+      </li>
+    </ul>
+  </div>
+);
 
 export default Details;
-
-{/* {this.props.products.map((product) =>
-  <Details
-    slogan={product.slogan}
-    />
-)} */}
