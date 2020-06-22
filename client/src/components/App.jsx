6547e89ddbe25a485da61c5fbc01d86eb1f5ead6
Zachery Converse <zacheryconverse@gmail.com>
2020-06-22 15:45:02 -0600

    hard code details

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,11 +7,18 @@ class App extends Component {
     super();
 
     this.state = {
-      products: [],
+      products: [{
+        id: 5,
+        name: 'Heir Force Ones',
+        slogan: 'A sneaker dynasty',
+        description: 'Now where da boxes where I keep mine? You should peep mine, maybe once or twice but never three times. I"m just a sneaker pro, I love Pumas and shell toes, but can"t nothin compare to a fresh crispy white pearl',
+        category: 'Kicks',
+        default_price: '99',
+      }],
       reviews: [],
     };
 
-    this.handleChange = this.handleChange.bind(this);
+    // this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -31,38 +38,41 @@ class App extends Component {
       .then((data) => this.setState({ reviews: data.results }));
   }
 
-  handleChange(event) {
-    const { value } = event.target;
-    this.setState(() => (
-      {
-        value,
-      }
-    ));
-  }
+  // handleChange(event) {
+  //   const { value } = event.target;
+  //   this.setState(() => (
+  //     {
+  //       value,
+  //     }
+  //   ));
+  // }
 
   render() {
     const { reviews, products } = this.state;
-    console.log('app products', products);
 
     return (
-      // <div>
-      //   <a className="header-logo" href="https://cdn.discordapp.com/attachments/722563828285440133/722566216157495317/Maze_Merchantile.png" />
-      // </div>
       <div>
-        <form>
-          <h1 className="container">Product Overview</h1>
-          <img alt="White shoes" src="https://images.unsplash.com/photo-1544441892-794166f1e3be?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80" />
-          <input
-            type="text"
-            value={products}
-            onChange={this.handleChange}
-          />
-          <Button />
-          <Details products={products.slogan} />
-        </form>
+        <h1 className="container">Product Overview</h1>
+        <Button />
+        {/* <Details products={products} reviews={reviews} /> */}
+        <Details />
       </div>
     );
   }
 }
 
 export default App;
+
+// <img alt="White shoes" src="https://images.unsplash.com/photo-1544441892-794166f1e3be?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80" />
+
+// <div>
+//   <a className="header-logo" href="https://cdn.discordapp.com/attachments/722563828285440133/722566216157495317/Maze_Merchantile.png" />
+// </div>
+
+// {/* <form> */}
+// {/* <input
+//   type="text"
+//   value={products}
+//   onChange={this.handleChange}
+// /> */}
+// {/* </form> */}
