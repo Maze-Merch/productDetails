6add1d463df53e4e66494b1202bead4b23466539
Zachery Converse <zacheryconverse@gmail.com>
2020-06-23 13:53:04 -0600

    react router

--- client/src/components/Screens/ProductScreen.jsx
+++ client/src/components/Screens/ProductScreen.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const ProductScreen = (props) => {
+  // console.log(props.match.params.id;)
   return <div>ProductScreen</div>;
 };
 
--- client/src/components/details/Details.jsx
+++ client/src/components/details/Details.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import App from '../App';
-import HomeScreen from '../Screens/HomeScreen';
-import ProductScreen from '../Screens/ProductScreen';
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import App from "../App";
+import HomeScreen from "../Screens/HomeScreen";
+import ProductScreen from "../Screens/ProductScreen";
 
 class Details extends React.Component {
   constructor() {
@@ -19,24 +19,29 @@ class Details extends React.Component {
   }
 
   getProductData() {
-    fetch('http://52.26.193.201:3000/products/list')
+    fetch("http://52.26.193.201:3000/products/list")
       .then((res) => res.json())
       .then((data) => this.setState({ products: data }));
-  }
+    }
 
-  getReviewData() {
-    fetch('http://52.26.193.201:3000/reviews/1/list')
+    getReviewData() {
+      fetch("http://52.26.193.201:3000/reviews/1/list")
       .then((res) => res.json())
       .then((data) => this.setState({ reviews: data.results }));
-  }
+    }
+
+  // const GetProductById = (props) => {
+  //   console.log(props.match.params.id);
+  // };
 
   render() {
     const { reviews, products } = this.state;
+    // console.log(props.match.params.id;)
 
     return (
       <BrowserRouter>
         <div className="content">
-          <Route path="/products/:id" component={ProductScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
           <Route path="/" exact={true} component={HomeScreen} />
           <ul className="products">
             {products.map((product) => (
@@ -47,14 +52,16 @@ class Details extends React.Component {
                   </div>
                   <div className="product-category">{product.category}</div>
                   <div className="product-name">
-                    <a href="product.html">{product.name}</a>
+                    <Link to={"/product/" + product.id}>{product.name}</Link>
                   </div>
                   <div className="product-price">${product.default_price}</div>
-                  <img
-                    className="product-image"
-                    src="https://images.unsplash.com/photo-1544441892-794166f1e3be?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80"
-                    alt={product.name}
-                  />
+                  <Link to={`/product/${product.id}`}>
+                    <img
+                      className="product-image"
+                      src="https://images.unsplash.com/photo-1544441892-794166f1e3be?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80"
+                      alt={product.name}
+                    />
+                  </Link>
                 </div>
               </li>
             ))}
@@ -65,9 +72,6 @@ class Details extends React.Component {
   }
 }
 
-const GetProductById = (props) => {
-  console.log(props.match.params.id);
-};
 
 export default Details;
 
